787111cfd287ccc7c6238c2f55018727bd5d4179
ru2nuts <alex@ru2nuts>
2018-02-28 15:03:40 -0500

    adding plugins for scalatest

--- src/test/scala/template/spark/PersonTest.scala
+++ src/test/scala/template/spark/PersonTest.scala
@@ -11,7 +11,7 @@ class PersonTest extends FlatSpec with Matchers with BeforeAndAfterEach {
   }
 
   "First name" should "be set during init" in {
-    log.debug("Testing First Name")
+    log.info("Testing First Name")
     val p = Person("John", "Smith", "USA", 100)
     p.firstName should be ("John")
   }
